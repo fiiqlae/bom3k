fb1f1ed94855336430b2cacee7952bdfbab55874
Maixm M. Kaganovich <fiiqlae@localhost.localdomain>
2020-12-14 04:26:16 +0300

    fixed SQL column indexes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import commonDefenitions.TransactionKind;
 import data.database.CredentialsManager;
 import data.database.CredentialsManagerImpl;
 import data.database.Database;
+import data.models.TransactionDataModel;
 import data.models.UserAccountDataModel;
 import di.modules.ApplicationModule;
 
@@ -12,6 +14,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,10 +24,31 @@ public class Main{
     public static void main(String[] args) {
         Injector injector = Guice.createInjector(new ApplicationModule());
         CredentialsManager c = injector.getInstance(CredentialsManager.class);
+        Database db = injector.getInstance(Database.class);
         try {
-            c.registerUser("pepman", "peppwd");
+            //c.registerUser("pepman", "peppwd");
             UserAccountDataModel uadm = c.logIn("pepman", "peppwd");
             System.out.println(uadm.getId());
+            TransactionDataModel tr = new TransactionDataModel(
+                    uadm.getId(),
+                    0,
+                    "pepe",
+                    TransactionKind.SPENDING,
+                    false,
+                    "0",
+                    "0",
+                    "pepe",
+                    "pepe",
+                    uadm.getUsername(),
+                    uadm.getUsername()
+            );
+            db.deleteTransaction(tr);
+            db.createTransaction(tr);
+            ArrayList<TransactionDataModel> t = db.selectUserTransactions(uadm.getId());
+            System.out.println("===");
+            for (TransactionDataModel tdm: t) {
+                System.out.println(tdm.getTransactionId());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
